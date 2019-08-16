968b20fd951e6e4ff31677a73ceddc33a7f9f994
Demi Ogun <demiogun1@gmail.com>
2019-08-16 15:20:08 +0100

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-  get 'doses/create'
-  get 'doses/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "cocktails#index"
   resources :cocktails do
     resources :doses, only: [:new, :create]
   end
