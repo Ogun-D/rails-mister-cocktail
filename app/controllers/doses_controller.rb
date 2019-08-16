6083e99d9b0ca80b7e73d72a6abc73be04be3144
Demi Ogun <demiogun1@gmail.com>
2019-08-16 08:45:23 +0100

    Pushing 'Cocktail' Challenge

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,29 @@
+class DosesController < ApplicationController
+  # before_action :set_cocktail, only: %i[new create]
+  # before_action :set_ingredient, only: %i[new create]
+
+
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    # @ingredient = Ingredient.find(params[:dose][:ingredient_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    # @dose.ingredient = @ingredient
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
